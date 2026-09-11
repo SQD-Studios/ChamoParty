80bf2a82399be4b3da7137a5933227dc2473e179
Chamogelastos <stavros.ssta@gmail.com>
2026-09-11 18:12:08 +0300

    Bump dependencies

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -18,8 +18,8 @@ dependencies {
 
     compileOnly("com.github.NuVotifier.NuVotifier:nuvotifier-api:2.7.2")
     compileOnly("com.github.NuVotifier.NuVotifier:nuvotifier-bukkit:2.7.2")
-    compileOnly("fr.maxlego08.menu:zmenu-api:1.1.1.4")
-    compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("fr.maxlego08.menu:zmenu-api:1.1.1.8")
+    compileOnly("me.clip:placeholderapi:2.12.3")
 
     compileOnly("net.strokkur.commands:annotations-paper:2.3.0")
     annotationProcessor("net.strokkur.commands:processor-paper:2.3.0")
@@ -31,7 +31,7 @@ tasks {
     runServer {
         downloadPlugins {
             github("NuVotifier", "NuVotifier", "v2.7.3", "nuvotifier.jar")
-            modrinth("XPQ42u1g", "1.1.1.7")
+            modrinth("XPQ42u1g", "1.1.1.8")
             github("PlaceholderAPI", "PlaceholderAPI", "2.12.3", "PlaceholderAPI-2.12.3.jar")
         }
 
